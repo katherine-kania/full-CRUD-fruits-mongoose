73f1b26032b09d2b2589cf22b7b80c1d4dadd6ab
katherine-kania <katherine.kania@gmail.com>
2022-03-09 11:50:17 -0500

    adds layout and index views

--- models/connection.js
+++ models/connection.js
@@ -1,12 +1,12 @@
-///////////////////////////////////////////
+/////////////////////////////////
 // import dependencies
-///////////////////////////////////////////
+/////////////////////////////////
 require('dotenv').config()
-const mongoose = require ('mongoose')
+const mongoose = require('mongoose')
 
-///////////////////////////////////////////
+/////////////////////////////////
 // database connection
-///////////////////////////////////////////
+/////////////////////////////////
 // here we are setting up inputs for our connect function
 const DATABASE_URL = process.env.DATABASE_URL
 const CONFIG = {
@@ -14,16 +14,16 @@ const CONFIG = {
     useUnifiedTopology: true
 }
 
-//establish connection
+// establish connection
 mongoose.connect(DATABASE_URL, CONFIG)
 
-// event for when our connection opens/ closes/ errors
+// events for when our connection opens/closes/errors
 mongoose.connection
-  .on('open', () => console.log('Connected to Mongoose'))
-  .on('close', () => console.log('Disconnected from Mongoose'))
-  .on('error', (error) => console.log(error))
+	.on('open', () => console.log('Connected to Mongoose'))
+	.on('close', () => console.log('Disconnected from Mongoose'))
+	.on('error', (error) => console.log(error))
 
-///////////////////////////////////////////
+/////////////////////////////////
 // export our connection
-///////////////////////////////////////////
-module.export = mongoose 
+/////////////////////////////////
+module.exports = mongoose
--- server.js
+++ server.js
@@ -5,7 +5,6 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const methodOverride = require('method-override')
-// we'll also import our fruits model when we have it
 const Fruit = require('./models/fruit')
 
 ///////////////////////////////////////////
@@ -30,6 +29,33 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
     res.send('the app is running, better go catch it')
 })
+
+app.get("/fruits/seed", (req, res) => {
+    // array of starter fruits
+    const startFruits = [
+        { name: 'Orange', color: 'orange', readyToEat: false },
+        { name: 'Grape', color: 'purple', readyToEat: false },
+        { name: 'Banana', color: 'orange', readyToEat: false },
+        { name: 'Strawberry', color: 'red', readyToEat: false },
+        { name: 'Coconut', color: 'brown', readyToEat: false },
+    ]
+
+    // when we seed data, there are a few steps involved 
+    // delete all the data that already exists (will only happen if data exists)
+    Fruit.remove({})
+        .then(data => {
+            console.log('this is what remove returns', data)
+            // then we create with our seed data
+            Fruit.create(startFruits)
+                .then(data => {
+                    console.log('this is what creat returns data', data)
+                    res.send(data)
+                })
+        })
+
+
+})
+
 
 
 
